201b7eccd646d680ab88f6f9a03271e4b9621b3e
ulrmdhn <auliarama965@gmail.com>
2021-07-16 00:02:17 +0700

    ulrmdhn.github.io

--- js/script.js
+++ js/script.js
@@ -1,26 +1,7 @@
-const txtElement = ['Web Developer', 'Graphic Designer']
-let count = 0;
-let txtIndex = 0;
-let currentTxt = '';
-let words = '';
-
-(function ngetik(){
-
-    if(count == txtElement.length){
-        count = 0;
-
+$(window).on("scroll", function () {
+    if ($(window).scrollTop() > 20) {
+        $(".heading").addClass("active");
+    } else {
+        $(".heading").removeClass("active");
     }
-
-    currentTxt = txtElement[count];
-
-    words = currentTxt.slice(0, ++txtIndex);
-    document.querySelector('.efek-ngetik').textContent = words;
-
-    if(words.length == currentTxt.length){
-        count++;
-        txtIndex = 0;
-    }
-
-    setTimeout(ngetik, 200);
-
-})();
+});
